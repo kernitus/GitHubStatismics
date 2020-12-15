c2b802ed2e3563c00d0012b553b8ee8bad551d25
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-15 20:35:58 +0100

    Add project skeleton & dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.kohsuke:github-api:1.117")
+}
---
+++ src/main/kotlin/GitHubStatismics.kt
@@ -0,0 +1,7 @@
+package main.kotlin
+
+import org.kohsuke.github.GitHub
+
+fun main(){
+    val github = GitHub.connect()
+}
