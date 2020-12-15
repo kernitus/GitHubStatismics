b6f3d8a64119ecf3ab2ed79141ff5038bb1d742b
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-15 22:38:39 +0100

    Print basic user data

--- src/main/kotlin/GitHubStatismics.kt
+++ src/main/kotlin/GitHubStatismics.kt
@@ -3,5 +3,22 @@ package main.kotlin
 import org.kohsuke.github.GitHub
 
 fun main(){
-    val github = GitHub.connect()
+    val github = GitHub.connectAnonymously()
+
+    println("Please enter a GitHub username:")
+    val username = readLine()
+
+    val user = github.getUser(username)
+    println("Username: ${user.name}")
+    println("Bio: ${user.bio}")
+    println("Organisations: ${user.organizations}")
+    println("Follower count: ${user.followersCount}")
+    println("Following count: ${user.followingCount}")
+
+    print("Followers: ")
+    user.followers.forEach { follower ->
+        if (follower.name != null)
+            print("\"${follower.name}\" ")
+    }
+    println()
 }
