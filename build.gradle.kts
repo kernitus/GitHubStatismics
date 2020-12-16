554a666ec42f0e5dd42ab0d6e2fe90fff1ac4ed5
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-16 12:35:36 +0100

    Add skeleton webpage UI

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,14 @@ plugins {
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     implementation("org.kohsuke:github-api:1.117")
+    implementation("com.github.kwebio:kweb-core:0.8.0")
+
+    // This (or another SLF4J binding) is required for Kweb to log errors
+    implementation(group = "org.slf4j", name = "slf4j-simple", version = "1.7.30")
 }
--- src/main/kotlin/GitHubStatismics.kt
+++ src/main/kotlin/GitHubStatismics.kt
@@ -1,8 +1,84 @@
 package main.kotlin
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.launch
+import kweb.*
+import kweb.plugins.fomanticUI.fomantic
+import kweb.plugins.fomanticUI.fomanticUIPlugin
+import kweb.state.KVar
 import org.kohsuke.github.GitHub
 
-fun main(){
+fun main() {
+
+    Kweb(port = 16097, debug = true, plugins = listOf(fomanticUIPlugin)) {
+
+        doc.head {
+            title().text("GitHub Statismics")
+        }
+
+        doc.body {
+            val usernameKVar = KVar("")
+
+            pageBorderAndTitle(usernameKVar) {
+                div(fomantic.content) {
+
+                    div(fomantic.ui.action.input) {
+                        h2(fomantic.ui.header).text("Please enter a GitHub username: ")
+                        val input = input(type = InputType.text, placeholder = "Username")
+                        input.value = usernameKVar
+                        input.on.keypress { ke ->
+                            if (ke.code == "Enter") {
+                                handleChooseUsername(input)
+                            }
+                        }
+                        button(fomantic.ui.button).text("Run").apply {
+                            on.click {
+                                handleChooseUsername(input)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+private fun ElementCreator<*>.pageBorderAndTitle(
+    username: KVar<String>,
+    content: ElementCreator<DivElement>.() -> Unit
+) {
+    div(fomantic.ui.main.container) {
+        div(fomantic.column) {
+            div(fomantic.ui.vertical.segment) {
+                div(fomantic.ui.message) {
+                    p().innerHTML(
+                        """
+                            A simple GitHub statistics visualiser. Enter a username below to see some statistics associated with the account.
+                            """
+                            .trimIndent()
+                    )
+                }
+            }
+
+            div(fomantic.ui.vertical.segment) {
+                h1(fomantic.ui.dividing.header).text(username)
+                content(this)
+            }
+        }
+    }
+}
+
+private fun handleChooseUsername(input: InputElement) {
+    GlobalScope.launch {
+        val newItemText = input.getValue().await()
+        println("New username: $newItemText")
+        input.setValue("")
+    }
+}
+
+fun getGitHub(): GitHub {
     val github = GitHub.connectAnonymously()
 
     println("Please enter a GitHub username:")
@@ -21,4 +97,6 @@ fun main(){
             print("\"${follower.name}\" ")
     }
     println()
+
+    return github
 }
